6d048c532d2d34f4130dddc8263c450e3ddf6676
hugues <hugues.tavenard@eleves.enpc.fr>
2016-12-26 01:29:26 +0100

    Add general_ucbn algo which handles general feedback graphs.

--- test_ucbmax.py
+++ test_ucbmax.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import bandits
 from exp3G import exp3G
-from ucbmaxn import ucb_max_n, ucbn
+from ucbmaxn import ucb_max_n, ucbn, general_ucbn
 
 
 arm_0 = bandits.bernoulliArm(0.2)
@@ -11,16 +11,19 @@ arm_2 = bandits.bernoulliArm(0.4)
 
 arms = [arm_0, arm_1, arm_2]
 
-E = np.array([[1, 1, 0],[0,1,1],[1,0,1]])
+# E = np.array([[1, 1, 1],[1,1,1],[1,1,1]])
 # E = np.array([[1, 0, 0],[0,1,0],[0,0,1]])
+E = np.array([[0, 1, 1],[1,0,1],[1,1,0]])
 
 
-n_steps = 100
+n_steps = 1000
 n_runs = 100
 loss = np.zeros(n_steps)
 for t in range(n_runs):
     # loss += ucbn(n_steps, arms, E)
-    loss += ucb_max_n(n_steps, arms, E)
+    # loss += ucb_max_n(n_steps, arms, E)
+    loss += general_ucbn(n_steps, arms, E)
+
 loss = loss / float(n_runs)
 
 optimal_loss = 0.6 * (np.arange(n_steps)+1)
@@ -29,9 +32,6 @@ regret = optimal_loss - np.cumsum(loss)
 
 plt.plot(regret)
 plt.show()
-
-
-
 
 
 
--- ucbmaxn.py
+++ ucbmaxn.py
@@ -32,7 +32,8 @@ def ucb_max_n(n_steps, arms, E):
         j = neighbors_i[np.argmax((X/O)[neighbors_i])]
 
         # Draw arm j and observe the rewards of the neighbors
-        for k in np.where(E[j,:] != 0)[0]:
+        neighbors_j = np.where(E[j,:] != 0)[0]
+        for k in neighbors_j:
             O[k] += 1
             rew = arms[k].sample()
             X[k] += rew
@@ -68,15 +69,67 @@ def ucbn(n_steps, arms, E):
         Y = X / O + np.sqrt(2.*np.log(t+K) / O)
         i = np.argmax(Y)
 
-        neighbors_i = np.where(E[:,i] != 0)[0]
+        neighbors_i = np.where(E[i,:] != 0)[0]
 
-        # Draw arm j and observe the rewards of the neighbors
+        # Draw arm i and observe the rewards of the neighbors
         for k in neighbors_i:
             O[k] += 1
             rew = arms[k].sample()
             X[k] += rew
             if k == i:
                 reward[t + K] = rew
+
+
+    return reward
+
+
+def general_ucbn(n_steps, arms, E):
+    """
+    Here we suppose that E does not have any 0 on the diagonal and is symmetric.
+    """
+    K = len(arms)
+    reward = np.zeros(n_steps)
+
+    # Initialize X : total rewards per arm
+    # O : number of observations per arm
+    # D : number of draws per arm
+    X = np.zeros(K)
+    O = np.zeros(K)
+    D = np.zeros(K)
+
+    # First draw each arm once
+    for k in range(K):
+        D[k] += 1
+        neighbors_k = np.where(E[k,:])[0]
+        for i in neighbors_k:
+            O[i] += 1
+            rew = arms[i].sample()
+            X[i] += rew
+            if i == k:
+                reward[k] = rew
+
+        # Handle the case when there are 0 on the diagonal
+        if k not in neighbors_k:
+            reward[k] = arms[k].sample()
+
+    # Do n_steps iterations of the alorithm
+    for t in range(n_steps - K):
+        Y = X / O + np.sqrt(2.*np.log(t+K) / D)
+        i = np.argmax(Y)
+        D[i] += 1
+        neighbors_i = np.where(E[i,:] != 0)[0]
+
+        # Draw arm i and observe the rewards of the neighbors
+        for k in neighbors_i:
+            O[k] += 1
+            rew = arms[k].sample()
+            X[k] += rew
+            if k == i:
+                reward[t + K] = rew
+
+        # Handle the case when there are 0 on the diagonal
+        if i not in neighbors_i:
+            reward[t + K] = arms[i].sample()
 
     return reward
 
