6108097c8b5e76e57c205ffbd846afa178b138fa
hugues <hugues.tavenard@eleves.enpc.fr>
2017-01-04 14:21:19 +0100

    Modify the expArm.

--- bandits.py
+++ bandits.py
@@ -11,26 +11,16 @@ class bernoulliArm():
     def sample(self):
         return int(random.random() < self.p)
 
-class gaussianArm():
-    def __init__(self, mu, sigma):
-        self.mean = mu
-        self.var = sigma
-
-    def sample(self):
-        R = random.gauss(self.mean, self.var)
-        return R*(R>0)*(R<1) + 1.0*(R>1)
-        return (random.gauss(self.mean, self.var))
 
 class expArm():
     def __init__(self, lambdap):
         self.lambdap = lambdap
-        self.mean = 1./self.lambdap
-        self.var = 1./(self.lambdap)**2
+        self.mean = (1./self.lambdap)*(1-np.exp(-self.lambdap))
+        self.var = 1
 
     def sample(self):
-        R = random.expovariate(self.lambdap)
-        return R*(R<1) + 1.0*(R>1)
-        #random.expovariate(self.lambdap)
+        return min(-1/self.lambdap*np.log(random.random()),1)
+
 
 class betaArm():
     def __init__(self, alpha, beta):
@@ -40,4 +30,3 @@ class betaArm():
         self.var = (alpha*beta)/float((alpha+beta)**2*(alpha+beta+1))
     def sample(self):
         return random.betavariate(self.alpha, self.beta)
-
