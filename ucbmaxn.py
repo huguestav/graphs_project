2702539a4db0c65d0706f405b916545d2be7e1c3
hugues <hugues.tavenard@eleves.enpc.fr>
2017-01-05 13:48:41 +0100

    Implement two new versions of the general ucb algo.

--- ucbmaxn.py
+++ ucbmaxn.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import numpy.matlib
 
 def ucb_max_n(n_steps, arms, E):
     """
@@ -79,11 +79,14 @@ def ucbn(n_steps, arms, E):
             if k == i:
                 reward[t + K] = rew
 
+        # Handle the case when there are 0 on the diagonal
+        if i not in neighbors_i:
+            reward[t + K] = arms[i].sample()
 
     return reward
 
 
-def general_ucbn(n_steps, arms, E):
+def general_ucbn_1(n_steps, arms, E):
     """
     Here we suppose that E does not have any 0 on the diagonal and is symmetric.
     """
@@ -133,6 +136,148 @@ def general_ucbn(n_steps, arms, E):
 
     return reward
 
+
+def general_ucbn_2(n_steps, arms, E):
+    """
+    Here we suppose that E does not have any 0 on the diagonal and is symmetric.
+    """
+    K = len(arms)
+    reward = np.zeros(n_steps)
+
+    # Initialize X : total rewards per arm
+    # O : number of observations per arm
+    # D : number of draws per arm
+    X = np.zeros(K)
+    O = np.zeros(K)
+    D = np.zeros(K)
+
+    # First draw each arm once
+    for k in range(K):
+        D[k] += 1
+        neighbors_k = np.where(E[k,:])[0]
+        for i in neighbors_k:
+            O[i] += 1
+            rew = arms[i].sample()
+            X[i] += rew
+            if i == k:
+                reward[k] = rew
+
+        # Handle the case when there are 0 on the diagonal
+        if k not in neighbors_k:
+            reward[k] = arms[k].sample()
+
+    # Do n_steps iterations of the alorithm
+    for t in range(n_steps - K):
+        # A = np.matlib.repmat(O,K,1) * (2 - E)
+        # D_arg = np.argmin(A, axis=1)
+        # D_val = np.min(A, axis=1)
+
+        D_val = np.zeros(K)
+        for k in range(K):
+            D_val[k] = np.min(O[np.where(E[k,:])[0]])
+
+        # print "A:", A
+        # print "repmat:", np.matlib.repmat(O,K,1)
+        # print "O:", O
+        # print "D:", D_val
+        # print ""
+
+        Y = X / O + np.sqrt(2.*np.log(t+K) / D_val)
+        i = np.argmax(Y)
+        neighbors_i = np.where(E[i,:] != 0)[0]
+
+        # Draw arm i and observe the rewards of the neighbors
+        for k in neighbors_i:
+            O[k] += 1
+            rew = arms[k].sample()
+            X[k] += rew
+            if k == i:
+                reward[t + K] = rew
+
+        # Handle the case when there are 0 on the diagonal
+        if i not in neighbors_i:
+            reward[t + K] = arms[i].sample()
+
+    return reward
+
+
+def general_ucbn_3(n_steps, arms, E):
+    """
+    Here we suppose that E does not have any 0 on the diagonal and is symmetric.
+    """
+    K = len(arms)
+    reward = np.zeros(n_steps)
+
+    # Initialize X : total rewards per arm
+    # O : number of observations per arm
+    # D : number of draws per arm
+    X = np.zeros(K)
+    O = np.zeros(K)
+    D = np.zeros(K)
+
+    # First draw each arm once
+    for k in range(K):
+        D[k] += 1
+        neighbors_k = np.where(E[k,:])[0]
+        for i in neighbors_k:
+            O[i] += 1
+            rew = arms[i].sample()
+            X[i] += rew
+            if i == k:
+                reward[k] = rew
+
+        # Handle the case when there are 0 on the diagonal
+        if k not in neighbors_k:
+            reward[k] = arms[k].sample()
+
+    # Do n_steps iterations of the alorithm
+    for t in range(n_steps - K):
+        # A = np.matlib.repmat(O,K,1) * (2 - E)
+        # D_arg = np.argmin(A, axis=1)
+        # D_val = np.min(A, axis=1)
+
+        D_arg = np.zeros(K, dtype=np.int)
+        for k in range(K):
+            D_arg[k] = np.argmin(O[np.where(E[k,:])[0]])
+        D_val = O[D_arg]
+
+        # print "A:", A
+        # print "repmat:", np.matlib.repmat(O,K,1)
+        # print "O:", O
+        # print "D:", D_val
+        # print ""
+
+        Y = X / O + np.sqrt(2.*np.log(t+K) / D_val)
+        i = np.argmax(Y)
+
+        # Neighbor of i with smallest number of observations (i -> k)
+        k = D_arg[i]
+        neighbors_k = np.where(E[k,:] != 0)[0]
+
+        # Consider as a candidate : j which is the neighbor of k
+        # with the highest expected value.
+        j = neighbors_k[np.argmax((X/O)[neighbors_k])]
+
+        # Select the best arm between i and j
+        Y = X / O
+        if Y[j] > Y[i]:
+            i = j
+
+        neighbors_i = np.where(E[i,:] != 0)[0]
+
+        # Draw arm i and observe the rewards of the neighbors
+        for k in neighbors_i:
+            O[k] += 1
+            rew = arms[k].sample()
+            X[k] += rew
+            if k == i:
+                reward[t + K] = rew
+
+        # Handle the case when there are 0 on the diagonal
+        if i not in neighbors_i:
+            reward[t + K] = arms[i].sample()
+
+    return reward
 
 
 def ucb1(n_steps, arms):
