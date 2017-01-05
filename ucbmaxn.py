7dff02521f50d154465c5861e5dd639543d54fb0
hugues <hugues.tavenard@eleves.enpc.fr>
2017-01-05 22:25:30 +0100

    Modify general ucb 2 and 3

--- ucbmaxn.py
+++ ucbmaxn.py
@@ -146,11 +146,14 @@ def general_ucbn_2(n_steps, arms, E):
 
     # Initialize X : total rewards per arm
     # O : number of observations per arm
+    # D : number of draws per arm
     X = np.zeros(K)
     O = np.zeros(K)
+    D = np.zeros(K)
 
     # First draw each arm once
     for k in range(K):
+        D[k] += 1
         neighbors_k = np.where(E[k,:])[0]
         for i in neighbors_k:
             O[i] += 1
@@ -167,13 +170,34 @@ def general_ucbn_2(n_steps, arms, E):
     for t in range(n_steps - K):
         D_val = np.zeros(K)
         for k in range(K):
-            D_val[k] = np.min(O[np.where(E[k,:])[0]])
+            non_zero_idx = np.where(E[k,:])[0]
+            if len(non_zero_idx):
+                # If k observes rewards, take the min number of rewards among
+                # the neighbors of k
+                D_val[k] = np.min(O[np.where(E[k,:])[0]])
+            else:
+                # Take the number of draws of k
+                D_val[k] = max(D[k], O[k])
+                # D_val[k] = D[k] + O[k]
+                # D_val[k] = O[k]
+                # D_val[k] = D[k]
+                # print O[k] == D[k]
+
+                # print "O:", O
+                # print "D:", D
+                # D_val[k] = float(D[k])
 
         Y = X / O + np.sqrt(2.*np.log(t+K) / D_val)
         i = np.argmax(Y)
         neighbors_i = np.where(E[i,:] != 0)[0]
 
+
+        # print "Y:", Y
+        # print "i:", i
+        # print ""
+
         # Draw arm i and observe the rewards of the neighbors
+        D[i] += 1
         for k in neighbors_i:
             O[k] += 1
             rew = arms[k].sample()
@@ -197,11 +221,14 @@ def general_ucbn_3(n_steps, arms, E):
 
     # Initialize X : total rewards per arm
     # O : number of observations per arm
+    # D : number of draws per arm
     X = np.zeros(K)
     O = np.zeros(K)
+    D = np.zeros(K)
 
     # First draw each arm once
     for k in range(K):
+        D[k] += 1
         neighbors_k = np.where(E[k,:])[0]
         for i in neighbors_k:
             O[i] += 1
@@ -217,12 +244,21 @@ def general_ucbn_3(n_steps, arms, E):
     # Do n_steps iterations of the alorithm
     for t in range(n_steps - K):
         D_arg = np.zeros(K, dtype=np.int)
+        D_val = np.zeros(K, dtype=np.int)
         for k in range(K):
             non_zero_idx = np.where(E[k,:])[0]
-            min_non_zero_idx = np.argmin(O[non_zero_idx])
-            D_arg[k] = non_zero_idx[min_non_zero_idx]
-        D_val = O[D_arg]
 
+            if len(non_zero_idx):
+                # If k observes rewards, take the min number of rewards among
+                # the neighbors of k
+                min_non_zero_idx = np.argmin(O[non_zero_idx])
+                D_arg[k] = non_zero_idx[min_non_zero_idx]
+                D_val[k] = O[D_arg[k]]
+            else:
+                # Take the number of draws of k
+                D_arg[k] = float(k)
+                D_val[k] = max(D[k], O[k])
+                # D_val[k] = D[k]
 
         Y = X / O + np.sqrt(2.*np.log(t+K) / D_val)
         i = np.argmax(Y)
@@ -230,19 +266,20 @@ def general_ucbn_3(n_steps, arms, E):
         # Neighbor of i with smallest number of observations (i -> k)
         k = D_arg[i]
         neighbors_k = np.where(E[:,k] != 0)[0]
+        if len(neighbors_k):
+            # Consider as a candidate : j which is the neighbor of k
+            # with the highest expected value.
+            j = neighbors_k[np.argmax((X/O)[neighbors_k])]
 
-        # Consider as a candidate : j which is the neighbor of k
-        # with the highest expected value.
-        j = neighbors_k[np.argmax((X/O)[neighbors_k])]
-
-        # Select the best arm between i and j
-        Y = X / O
-        if Y[j] > Y[i]:
-            i = j
+            # Select the best arm between i and j
+            Y = X / O
+            if Y[j] > Y[i]:
+                i = j
 
         neighbors_i = np.where(E[i,:] != 0)[0]
 
         # Draw arm i and observe the rewards of the neighbors
+        D[i] += 1
         for k in neighbors_i:
             O[k] += 1
             rew = arms[k].sample()
